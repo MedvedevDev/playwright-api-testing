671d4870e0492644035933c0d2c8880acaa7335c
MedvedevDev <a95.medvedev@gmail.com>
2026-02-03 17:32:09 +0100

    change article title and description via mocked date

--- tests/interceptAPI.spec.ts
+++ tests/interceptAPI.spec.ts
@@ -1,20 +1,45 @@
 import { test } from "@playwright/test";
+import tags from '../test-data/tags.json'
 
-test("mocking tags on the website", async ({ page }) => {
+test.use({
+  launchOptions: {
+    args: [
+      "--disable-http-cache",
+      "--disable-service-worker",
+    ],
+  },
+});
+
+
+
+test("intercept and mock tags API", async ({ page }) => {
   await page.route(
     "*/**/api/tags",
     async (route) => {
-      const tags = {
-        "tags" : [
-          "automation",
-          "play"
-        ]
-      }
       await route.fulfill({
         body: JSON.stringify(tags),
       });
     },
   );
 
-  await page.goto("https://angular.realworld.io/");
+  await page.goto("https://conduit.bondaracademy.com/");
 });
+
+
+test("intercept and intercept articles API", async({page})=>{
+  await page.route("*/**/api/articles*", async route => {
+    const response = await route.fetch();
+    const responseBody = await response.json(); // array of objects: each object has slug, title, descr, body and etc.
+    
+    console.log(responseBody.articles[0])
+    responseBody.articles[0].title = "NEW TITLE"
+    responseBody.articles[0].description = "THIS IS A DESCROPTION"
+
+    // apply new title and description to the very first atricle at the top
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+  })
+
+  await page.goto("https://conduit.bondaracademy.com/");
+})
